ae26ab329432565fb6b14910d5294debcb04e87d
Andres Oliveira <andresoliveira@protonmail.com>
2024-09-04 20:37:58 -0300

    add: index layout

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // express configuration
 const express = require("express")
+const bodyParser = require("body-parser")
 const app = express()
+
+// body parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 // ejs configuration
 app.set('view engine', 'ejs')
@@ -13,6 +18,20 @@ app.get('/', (req, res) => {
 
 app.get('/ask', (req, res) => {
   res.render("pages/ask")
+})
+
+app.post('/create', (req, res) => {
+  let title = req.body.title
+  let description = req.body.description
+  let id = 0
+
+  res.send(
+    {
+      'id': id++,
+      'title': title,
+      'description': description
+    }
+  )
 })
 
 // listen
